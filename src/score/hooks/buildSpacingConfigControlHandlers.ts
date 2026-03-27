70233125730283de4266edaaf76b98c56baa472d
ZHOUAN <767432809@qq.com>
2026-03-27 12:30:28 +0800

    refactor: split score view props adapters

---
+++ src/score/hooks/buildSpacingConfigControlHandlers.ts
@@ -0,0 +1,107 @@
+import { DEFAULT_TIME_AXIS_SPACING_CONFIG } from '../layout/timeAxisSpacing'
+import {
+  DEFAULT_CHORD_MARKER_PADDING_PX,
+  DEFAULT_CHORD_MARKER_UI_SCALE_PERCENT,
+  DEFAULT_PAGE_HORIZONTAL_PADDING_PX,
+  clampBaseMinGap32Px,
+  clampChordMarkerPaddingPx,
+  clampChordMarkerUiScalePercent,
+  clampDurationGapRatio,
+  clampLeadingBarlineGapPx,
+  clampPageHorizontalPaddingPx,
+  clampSecondChordSafeGapPx,
+} from '../scorePresentation'
+import type { ScoreViewAdapterParams } from './scoreViewAdapterTypes'
+
+export function buildSpacingConfigControlHandlers(params: {
+  appState: ScoreViewAdapterParams['appState']
+}) {
+  const { appState } = params
+  const {
+    setPageHorizontalPaddingPx,
+    setChordMarkerUiScalePercent,
+    setChordMarkerPaddingPx,
+    setTimeAxisSpacingConfig,
+  } = appState
+
+  return {
+    onPageHorizontalPaddingPxChange: (nextValue: number) =>
+      setPageHorizontalPaddingPx(clampPageHorizontalPaddingPx(nextValue)),
+    onChordMarkerUiScalePercentChange: (nextValue: number) =>
+      setChordMarkerUiScalePercent(clampChordMarkerUiScalePercent(nextValue)),
+    onChordMarkerPaddingPxChange: (nextValue: number) =>
+      setChordMarkerPaddingPx(clampChordMarkerPaddingPx(nextValue)),
+    onBaseMinGap32PxChange: (nextValue: number) =>
+      setTimeAxisSpacingConfig((current) => ({
+        ...current,
+        baseMinGap32Px: clampBaseMinGap32Px(nextValue),
+      })),
+    onLeadingBarlineGapPxChange: (nextValue: number) =>
+      setTimeAxisSpacingConfig((current) => ({
+        ...current,
+        leadingBarlineGapPx: clampLeadingBarlineGapPx(nextValue),
+      })),
+    onSecondChordSafeGapPxChange: (nextValue: number) =>
+      setTimeAxisSpacingConfig((current) => ({
+        ...current,
+        secondChordSafeGapPx: clampSecondChordSafeGapPx(nextValue),
+      })),
+    onDurationGapRatio32Change: (nextValue: number) =>
+      setTimeAxisSpacingConfig((current) => ({
+        ...current,
+        durationGapRatios: {
+          ...current.durationGapRatios,
+          thirtySecond: clampDurationGapRatio(nextValue),
+        },
+      })),
+    onDurationGapRatio16Change: (nextValue: number) =>
+      setTimeAxisSpacingConfig((current) => ({
+        ...current,
+        durationGapRatios: {
+          ...current.durationGapRatios,
+          sixteenth: clampDurationGapRatio(nextValue),
+        },
+      })),
+    onDurationGapRatio8Change: (nextValue: number) =>
+      setTimeAxisSpacingConfig((current) => ({
+        ...current,
+        durationGapRatios: {
+          ...current.durationGapRatios,
+          eighth: clampDurationGapRatio(nextValue),
+        },
+      })),
+    onDurationGapRatio4Change: (nextValue: number) =>
+      setTimeAxisSpacingConfig((current) => ({
+        ...current,
+        durationGapRatios: {
+          ...current.durationGapRatios,
+          quarter: clampDurationGapRatio(nextValue),
+        },
+      })),
+    onDurationGapRatio2Change: (nextValue: number) =>
+      setTimeAxisSpacingConfig((current) => ({
+        ...current,
+        durationGapRatios: {
+          ...current.durationGapRatios,
+          half: clampDurationGapRatio(nextValue),
+        },
+      })),
+    onDurationGapRatioWholeChange: (nextValue: number) =>
+      setTimeAxisSpacingConfig((current) => ({
+        ...current,
+        durationGapRatios: {
+          ...current.durationGapRatios,
+          whole: clampDurationGapRatio(nextValue),
+        },
+      })),
+    onResetSpacingConfig: () => {
+      setTimeAxisSpacingConfig({
+        ...DEFAULT_TIME_AXIS_SPACING_CONFIG,
+        durationGapRatios: { ...DEFAULT_TIME_AXIS_SPACING_CONFIG.durationGapRatios },
+      })
+      setPageHorizontalPaddingPx(DEFAULT_PAGE_HORIZONTAL_PADDING_PX)
+      setChordMarkerUiScalePercent(DEFAULT_CHORD_MARKER_UI_SCALE_PERCENT)
+      setChordMarkerPaddingPx(DEFAULT_CHORD_MARKER_PADDING_PX)
+    },
+  }
+}
---
+++ src/score/hooks/scoreViewAdapterTypes.ts
@@ -0,0 +1,21 @@
+import type { ComponentProps } from 'react'
+import { ScoreBoard } from '../components/ScoreBoard'
+import { ScoreControls } from '../components/ScoreControls'
+import { useScoreAppState } from './useScoreAppState'
+import { useScoreEditorRefs } from './useScoreEditorRefs'
+import { useHorizontalScoreLayout } from './useHorizontalScoreLayout'
+import { useScoreCoreEditingController } from './useScoreCoreEditingController'
+import { useScoreInteractionRuntimeController } from './useScoreInteractionRuntimeController'
+
+export type ScoreControlsProps = ComponentProps<typeof ScoreControls>
+export type ScoreBoardProps = ComponentProps<typeof ScoreBoard>
+
+export type ScoreViewAdapterParams = {
+  appState: ReturnType<typeof useScoreAppState>
+  editorRefs: ReturnType<typeof useScoreEditorRefs>
+  layout: ReturnType<typeof useHorizontalScoreLayout>
+  chordMarker: ReturnType<typeof useScoreCoreEditingController>['chordMarker']
+  workspace: ReturnType<typeof useScoreInteractionRuntimeController>['workspace']
+  editorUi: ReturnType<typeof useScoreInteractionRuntimeController>['editorUi']
+  playback: ReturnType<typeof useScoreInteractionRuntimeController>['playback']
+}
